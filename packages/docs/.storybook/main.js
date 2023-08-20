bb85f598ae84229e1c9fee5c733ca89e95bff635
brunoramux <bruno.lemos@live.com>
2023-08-19 21:04:40 -0300

    Atualizacao de versoes de dependencias

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,6 @@ const config = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath('storybook-dark-mode')
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
