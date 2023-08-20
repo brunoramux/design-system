c8ebe5981797d85d29fa62e3e92653c73d240cee
brunoramux <bruno.lemos@live.com>
2023-08-19 23:12:22 -0300

    Atualizando dependencias

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@brunoramos-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
